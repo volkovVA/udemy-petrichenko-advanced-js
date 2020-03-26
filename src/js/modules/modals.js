4a664ff73439da6626abe30c8e45da1b05e16a04
Volkov Slava <volkov.slava@hotmail.com>
2020-03-26 19:35:55 +0300

    added frontend tabs

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -38,7 +38,7 @@ const modals = () => {
 
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup .popup_close');
-  showModalByTime('.popup', 60000);
+  // showModalByTime('.popup', 60000);
 
 };
 
