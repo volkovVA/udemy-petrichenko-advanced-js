db9219e6865678082bd5ef003be6aaf840f5b127
Volkov Slava <volkov.slava@hotmail.com>
2020-03-26 17:21:24 +0300

    added frontend modal window

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,3 +1,6 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
 $(document).ready(function() {
     $('.glazing_slider').slick({
         infinite: true,
@@ -67,4 +70,4 @@ $(document).ready(function() {
             }
         }]
     });
-});   
+});
