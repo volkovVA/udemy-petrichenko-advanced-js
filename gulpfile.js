950e2bb0bedfeb6db164c2510b5ebdeb3bb2c0b7
Volkov Slava <volkov.slava@hotmail.com>
2020-03-27 10:16:02 +0300

    added frontend form of working with data

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-const dist = "./dist/";
+// const dist = "./dist/";
+const dist = "C:/Program Files/Web/OSPanel/domains/test";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
@@ -56,7 +57,7 @@ gulp.task("watch", () => {
 		port: 4000,
 		notify: true
     });
-    
+
     gulp.watch("./src/index.html", gulp.parallel("copy-html"));
     gulp.watch("./src/assets/**/*.*", gulp.parallel("copy-assets"));
     gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
