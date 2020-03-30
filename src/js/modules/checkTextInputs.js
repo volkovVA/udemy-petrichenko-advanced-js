a4339c84ddf6c7b33f01cd619b06849772707725
Volkov Slava <volkov.slava@hotmail.com>
2020-03-30 18:24:06 +0300

    added frontend mask & validation of сyrillic characters

---
+++ src/js/modules/checkTextInputs.js
@@ -0,0 +1,18 @@
+const checkTextInputs = (selector) => {
+  const txtInputs = document.querySelectorAll(selector);
+
+  txtInputs.forEach(input => {
+    input.addEventListener('keypress', (e) => {
+      if (e.key.match(/[^а-яё 0-9]/ig)) {
+        e.preventDefault();
+      }
+    });
+    input.addEventListener('input', () => {
+      if (input.value.match(/[^а-яё 0-9]/ig)) {
+        input.value = '';
+      }
+    });
+  });
+};
+
+export default checkTextInputs;
