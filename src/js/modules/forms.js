bb390b31dfc3c6b830e87d6616bc6319ad807f5c
Volkov Slava <volkov.slava@hotmail.com>
2020-03-30 19:34:27 +0300

    added frontend loading elements

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,12 +1,10 @@
-// import checkNumInputs from './checkNumInputs';
+import { postData } from '../services/requests';
 
 const forms = () => {
   const form = document.querySelectorAll('form'),
         inputs = document.querySelectorAll('input'),
         windows = document.querySelectorAll('[data-modal]'),
         upload = document.querySelectorAll('[name="upload"]');
-
-  // checkNumInputs('input[name="user_phone"]');
 
   const message = {
     loading: 'Загрузка...',
@@ -20,15 +18,6 @@ const forms = () => {
   const path = {
     designer: 'assets/server.php',
     question: 'assets/question.php'
-  };
-
-  const postData = async (url, data) => {
-    let res = await fetch(url, {
-      method: 'POST',
-      body: data
-    });
-
-    return await res.text();
   };
 
   function clearInputs() {
