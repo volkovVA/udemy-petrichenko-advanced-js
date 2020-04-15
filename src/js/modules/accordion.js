728084fde9dcfced0e37160cf5f65a7b31ac5b00
Volkov Slava <volkov.slava@hotmail.com>
2020-04-15 12:38:47 +0300

    added accordion

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,28 @@
+const accordion = (triggersSelector) => {
+  const btns = document.querySelectorAll(triggersSelector);
+
+  btns.forEach(btn => {
+
+    btn.addEventListener('click', function() {
+      btns.forEach(btn => {
+        if (btn.classList.contains('active-style')) {
+          btn.classList.remove('active-style');
+          btn.nextElementSibling.classList.remove('active-content');
+          btn.nextElementSibling.style.maxHeight = '0px';
+        }
+      });
+
+      this.classList.toggle('active-style');
+      this.nextElementSibling.classList.toggle('active-content');
+
+      if (this.classList.contains('active-style')) {
+        this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight +80 + 'px';
+      } else {
+        this.nextElementSibling.style.maxHeight = '0px';
+      }
+    });
+  });
+
+};
+
+export default accordion;
