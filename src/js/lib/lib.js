75cd6a5c5e517e655b809fcd141080093d63a77c
Volkov Slava <volkov.slava@hotmail.com>
2020-04-19 14:24:26 +0300

    added animation

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -5,5 +5,6 @@ import './modules/classes';
 import './modules/handlers';
 import './modules/attributes';
 import './modules/actions';
+import './modules/effects';
 
 export default $;
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,1 @@
 import $ from './lib/lib';
-
