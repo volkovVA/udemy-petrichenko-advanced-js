71af022a4123994d63020d6bf1f7506ec3000d66
Volkov Slava <volkov.slava@hotmail.com>
2020-07-24 18:08:08 +0300

    editing README.md

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
 import $ from './lib/lib';
 
 $('#first').on('click', () => {
-  $('div').eq(1).fadeToggle(800);
+  $('.text').eq(0).fadeToggle(800);
 })
 
 $('[data-count="second"]').on('click', () => {
-  $('div').eq(2).fadeToggle(800);
+  $('.text').eq(0).fadeToggle(800);
 })
 
-$('button').eq(2).on('click', () => {
-  $('.w-500').fadeToggle(800);
+$('button').eq(0).on('click', () => {
+  $('.text').fadeToggle(800);
 })
 
 $('.wrap').html(
