cf44820e3c573584d2d29b2c66db32b38b1226c4
Volkov Slava <volkov.slava@hotmail.com>
2020-07-24 17:59:56 +0300

    editing README.md

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,7 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-// const dist = "./dist/";
-const dist = "C:/Program Files/Web/OSPanel/domains/test2";
+const dist = "./dist/";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
